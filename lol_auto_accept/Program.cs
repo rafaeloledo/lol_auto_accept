f1241cfa26e3d0a67e30eb94740cc5a01311a8f7
rafaeloledo <rafaeloliveiraledo@gmail.com>
2024-03-17 13:07:22 -0300

    first commit

---
+++ lol_auto_accept/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lol_auto_accept
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
